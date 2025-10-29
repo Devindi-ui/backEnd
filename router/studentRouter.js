171ebf5b58182ced96a4b14501dd3f8a8c797162
Devindi-u <panchalidevindi@gmail.com>
2025-10-29 16:06:02 +0530

    delete student

--- router/studentRouter.js
+++ router/studentRouter.js
@@ -6,5 +6,6 @@ router.post('/create', students.createStudent);
 router.get('/all', students.getAllStudents);
 router.get('/find/:id', students.getStudentById);
 router.put('/update/:id', students.updateStudent);
+router.delete('/delete/:id', students.deleteStudent);
 
 module.exports = router;
